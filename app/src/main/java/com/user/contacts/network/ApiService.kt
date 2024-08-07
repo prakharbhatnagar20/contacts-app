154b55fddb94ef3679f41ac77a66a4efc6029645
Prakhar Bhatnagar <92019275+prakharbhatnagar20@users.noreply.github.com>
2024-08-07 12:51:24 +0530

    first commit

--- app/src/main/java/com/user/contacts/network/ApiService.kt
+++ app/src/main/java/com/user/contacts/network/ApiService.kt
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 
 interface ContactsApi {
-    @GET("v0/b/user-contacts-ade83.appspot.com/o/contacts.json?alt=media&token=3e76df8d-7482-4e97-b987-f73d4283c0c5")
+    @GET("v0/b/user-contacts-ade83.appspot.com/o/contacts.json?alt=media&token=56ff997d-b2d1-41bd-8ba7-0e15de0bbfb0")
     suspend fun getContacts(): List<Contacts>
 }
 
